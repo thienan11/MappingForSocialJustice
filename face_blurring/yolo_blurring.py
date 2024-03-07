6884076b7333c0b52aba1ec080c7bd02ec0b64d9
ttran259 <ttran259@calpoly.edu>
2024-03-06 23:09:00 -0800

    attempt at applying pixelation to faces & reindent

--- face_blurring/yolo_blurring.py
+++ face_blurring/yolo_blurring.py
@@ -3,86 +3,109 @@ import torch
 from ultralytics import YOLO
 import tempfile
 import argparse
+import numpy as np
+
 
 # Determine the codec based on file extension
 def get_codec(filename):
-    extension = filename.split('.')[-1].lower()
-    if extension in ['avi']:
-        return 'XVID'
-    elif extension in ['mp4']:
-        return 'mp4v'
-    elif extension in ['mov']:
-        return 'avc1'
-    else:
-        raise ValueError("Unsupported file format")
+  extension = filename.split('.')[-1].lower()
+  if extension in ['avi']:
+      return 'XVID'
+  elif extension in ['mp4']:
+      return 'mp4v'
+  elif extension in ['mov']:
+      return 'avc1'
+  else:
+      raise ValueError("Unsupported file format")
+
+
+# Apply a strong pixelation effect to a region of the image.
+def apply_pixelation(image, xmin, ymin, xmax, ymax, pixel_size=5):
+  roi = image[ymin:ymax, xmin:xmax]
+  height, width = roi.shape[:2]
+  # Resize input to "pixelated" size with a very low resolution
+  temp_image = cv2.resize(roi, (pixel_size, pixel_size), interpolation=cv2.INTER_LINEAR)
+  # Scale back to original size
+  pixelated_image = cv2.resize(temp_image, (width, height), interpolation=cv2.INTER_NEAREST)
+  image[ymin:ymax, xmin:xmax] = pixelated_image
+
 
 # Processing and adding blur to images
 def process_image(model, input_path, output_path):
-    img = cv2.imread(input_path)
-    results = model(input_path, conf=0.05)
-    for detections in results:
-        if isinstance(detections, torch.Tensor):
-            for detection in detections:
-                xmin, ymin, xmax, ymax, conf, class_id = detection
-                xmin, ymin, xmax, ymax = map(int, [xmin, ymin, xmax, ymax])
-                roi = img[ymin:ymax, xmin:xmax]
-                blurred_roi = cv2.GaussianBlur(roi, (23, 23), 30)
-                img[ymin:ymax, xmin:xmax] = blurred_roi
-    cv2.imwrite(output_path, img)
+  img = cv2.imread(input_path)
+  results = model(input_path, conf=0.05)
+  for detections in results:
+    if isinstance(detections, torch.Tensor):
+      for detection in detections:
+        xmin, ymin, xmax, ymax, conf, class_id = detection
+        xmin, ymin, xmax, ymax = map(int, [xmin, ymin, xmax, ymax])
+
+        # roi = img[ymin:ymax, xmin:xmax]
+        # blurred_roi = cv2.GaussianBlur(roi, (23, 23), 30)
+        # img[ymin:ymax, xmin:xmax] = blurred_roi
+
+        apply_pixelation(img, xmin, ymin, xmax, ymax)
+
+  cv2.imwrite(output_path, img)
+
 
 # Processing and adding blur to videos
 def process_video(model, input_path, output_path):
-    cap = cv2.VideoCapture(input_path)
-    if not cap.isOpened():
-        print("Error opening video file.")
-        exit()
+  cap = cv2.VideoCapture(input_path)
+  if not cap.isOpened():
+    print("Error opening video file.")
+    exit()
 
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
+  frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+  frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+  frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
 
-    # fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-    # out = cv2.VideoWriter(output_path, fourcc, frame_rate, (frame_width, frame_height))
-    codec = get_codec(output_path)
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*codec), 20.0, (frame_width, frame_height))
+  # fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+  # out = cv2.VideoWriter(output_path, fourcc, frame_rate, (frame_width, frame_height))
+  codec = get_codec(output_path)
+  out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*codec), 20.0, (frame_width, frame_height))
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+  while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-        with tempfile.NamedTemporaryFile(suffix='.jpg') as tmpfile:
-            cv2.imwrite(tmpfile.name, frame)
-            results = model(tmpfile.name, conf=0.05)
-            for detections in results:
-                if isinstance(detections, torch.Tensor):
-                    for detection in detections:
-                        xmin, ymin, xmax, ymax, conf, class_id = detection
-                        xmin, ymin, xmax, ymax = map(int, [xmin, ymin, xmax, ymax])
-                        roi = frame[ymin:ymax, xmin:xmax]
-                        blurred_roi = cv2.GaussianBlur(roi, (23, 23), 30)
-                        frame[ymin:ymax, xmin:xmax] = blurred_roi
+    with tempfile.NamedTemporaryFile(suffix='.jpg') as tmpfile:
+      cv2.imwrite(tmpfile.name, frame)
+      results = model(tmpfile.name, conf=0.05)
+      for detections in results:
+        if isinstance(detections, torch.Tensor):
+          for detection in detections:
+            xmin, ymin, xmax, ymax, conf, class_id = detection
+            xmin, ymin, xmax, ymax = map(int, [xmin, ymin, xmax, ymax])
 
-            out.write(frame)
+            # roi = frame[ymin:ymax, xmin:xmax]
+            # blurred_roi = cv2.GaussianBlur(roi, (23, 23), 30)
+            # frame[ymin:ymax, xmin:xmax] = blurred_roi
 
-    cap.release()
-    out.release()
+            apply_pixelation(frame, xmin, ymin, xmax, ymax)
+
+      out.write(frame)
+
+  cap.release()
+  out.release()
+
 
 # Run program
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Add face blurring to images or videos with YOLOv8 model.")
-    parser.add_argument("-i", "--input", required=True, help="Input file path")
-    parser.add_argument("-m", "--mode", required=True, choices=["image", "video"], help="Processing mode: image or video")
-    parser.add_argument("-o", "--output", required=True, help="Output file path")
-    # parser.add_argument("--model", required=True, help="Path to the YOLO model")
+  parser = argparse.ArgumentParser(description="Add face blurring to images or videos with YOLOv8 model.")
+  parser.add_argument("-i", "--input", required=True, help="Input file path")
+  parser.add_argument("-m", "--mode", required=True, choices=["image", "video"], help="Processing mode: image or video")
+  parser.add_argument("-o", "--output", required=True, help="Output file path")
+  # parser.add_argument("--model", required=True, help="Path to the YOLO model")
 
-    args = parser.parse_args()
+  args = parser.parse_args()
 
-    # Load the YOLOv8 model
-    # model = YOLO(args.model)
-    model = YOLO('../models/yolov8n-face.pt')
+  # Load the YOLOv8 model
+  # model = YOLO(args.model)
+  model = YOLO('../models/yolov8n-face.pt')
 
-    if args.mode == "image":
-        process_image(model, args.input, args.output)
-    elif args.mode == "video":
-        process_video(model, args.input, args.output)
+  if args.mode == "image":
+    process_image(model, args.input, args.output)
+  elif args.mode == "video":
+    process_video(model, args.input, args.output)
