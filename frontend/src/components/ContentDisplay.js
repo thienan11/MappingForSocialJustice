4a733651a51ef8c7b3d38ebd0116b0ee213248c1
ttran259 <ttran259@calpoly.edu>
2024-05-23 01:10:51 -0700

    fix styling for ContentDisplay

--- frontend/src/components/ContentDisplay.js
+++ frontend/src/components/ContentDisplay.js
@@ -9,9 +9,9 @@ const ContentDisplay = ({ url, title, description, onClose }) => {
             <h3>{title}</h3>
             <p>{description}</p>
             {url.endsWith('.mp4') ? (
-                <video controls src={url} />
+                <video controls src={url} className="media-content" />
             ) : (
-                <img src={url} alt={title} />
+                <img src={url} alt={title} className="media-content" />
             )}
         </div>
     );
