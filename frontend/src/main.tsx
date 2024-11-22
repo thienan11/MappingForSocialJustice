1c651322965e36c083bd12e12ba9ffda1f9467ac
Thien An Tran <fxthienan@gmail.com>
2024-11-21 23:15:13 -0800

    refactor: replace BrowserRouter w/ createBrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from "react-router-dom";
+import { router } from './router'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
